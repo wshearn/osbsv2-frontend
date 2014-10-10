f466708af22ec9f21a10e71d35eada50d9e97cf5
Wesley Hearn <whearn@redhat.com>
2014-10-10 01:04:24 -0400

    Create random admin token

--- config/initializers/10_populatedata.js
+++ config/initializers/10_populatedata.js
@@ -1,6 +1,10 @@
 "use strict";
 
-var Group = require('../../app/models/Group');
+var mongoose = require('mongoose'),
+    crypto   = require('crypto');
+
+var Group = mongoose.model('Group'),
+    Token = mongoose.model('Token');
 
 function createAdminGroup(err, group) {
   if (err || !group) {
@@ -8,7 +12,24 @@ function createAdminGroup(err, group) {
       group: "admin"
     });
     adminGroup.save();
+    createAdminToken(adminGroup.id);
   }
+}
+
+function createAdminToken(adminId) {
+  // Generate a random 16 char one use admin id
+  crypto.randomBytes(8, function(err, buf){
+    var adminToken = new Token({
+      token: buf.toString('hex'),
+      groups: [adminId],
+      timesUsed: 0,
+      maxUse: 1
+    });
+
+    adminToken.save();
+
+    console.log("Admin Token: " + adminToken.token);
+  });
 }
 
 module.exports = function() {
